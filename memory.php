aa0cc66502c858b61be26a34e0fca87ba653ceea
admin <admin@MacBook-Pro-Lucas.local>
2022-10-27 14:45:12 +0200

    ajustement

--- memory.php
+++ memory.php
@@ -22,11 +22,39 @@
 </head>
 
 <body class="body">
-<?php
-    require_once "assets/view/header.inc.php";
-    ?>
+<?php require_once "assets/view/header.inc.php";?>
 
-    
+    <div class="tchat">
+        <div class="div-tchat">
+            <div class="header-tchat">
+                <h3>Chat</3>
+            </div>
+            <div class="body-tchat">
+                <div class="text-tchat-user">
+                    <img src="../assets/images/user.png" alt="">
+                    <div class>
+                        <p class="info-tchat">Moi</p>
+                        <p id="p1">Bsoir</p>
+                        <p class="info-tchat">Aujourd'hui à 10h30</p>
+                    </div>
+                </div>
+                <div class="text-tchat-response">
+                        <img src="../assets/images/user.png" alt="">
+                        <div>
+                            <p class="info-tchat">Other</p>
+                            <p id="p2">Wsh</p>
+                            <p class="info-tchat">Aujourd'hui à 11h00</p>
+                        </div>
+                </div>
+            </div>   
+            <div class="footer-chat">
+                <div class="input">
+                    <input type="text" placeholder="Votre message">
+                    <input class="footer-button" type="submit" value="Envoyer">
+                </div>
+            </div>
+        </div>    
+    </div>
 
     <?php
     require_once "assets/view/footer.inc.php";
